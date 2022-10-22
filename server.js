84130bce641dd3e1d571c3b3a988e97b087381b6
Res0ution <maestro@hotger.com>
2022-10-22 20:08:50 +0300

    setup db

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const authRoutes = require('./routes/authRoutes')
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("api/auth", authRoutes);
+
+console.log('Starting our server')
+
+const server = http.createServer(app)
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+      server.listen(PORT, () => {
+          console.log(`Listening on port ${PORT}`)
+      })
+  })
+  .catch((err) => {
+      console.log(`Database connection failed. Server not started.`)
+      console.error(err)
+  })
